21feaf6920caa457e5c9069d1085fb85c30fdbea
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2021-12-14 19:31:19 +0530

    Update generate sub arrays.cpp

--- Arrays/generate sub arrays.cpp
+++ Arrays/generate sub arrays.cpp
@@ -22,3 +22,47 @@ int main()
     return 0;
     
 }
+
+//find the maximum sub array sum and print the sub array
+#include <iostream>
+#include<climits>
+using namespace std;
+int main() 
+{
+    int arr[4] = {1,2,3};
+    int n= sizeof(arr)/sizeof(int);
+
+    int s=0;
+    int e=0;
+    
+    int max_sum = INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            int sum = 0;
+            for(int k=i;k<=j;k++)
+            {
+                sum +=arr[k];
+            }
+            if(sum>max_sum)
+            {
+              max_sum = sum;
+              s = i;
+              e = j;
+            }
+        }
+    }
+
+    cout<<"maximum sum is "<<max_sum<<endl;
+    cout<<"maximum sub array is "<<endl;
+
+    for(int i=s;i<=e;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+    
+}
+
