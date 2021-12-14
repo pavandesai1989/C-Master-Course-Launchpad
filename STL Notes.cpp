99a61866448d0d4a5f7c8d66dc8ee0179857efdb
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2021-12-14 21:25:52 +0530

    Create STL Notes.cpp

---
+++ STL Notes.cpp
@@ -0,0 +1,21 @@
+/*
+ generic programming using templates
+ 1. containers ( example vectors )
+ 2. algorithm
+ 
+ 2. algorithm : write linear search function and make it generic using templates.
+ 
+ containers in STL: Explore 
+ 1. sequence containers -- arrays,vector,list,deque,forward_list
+ 2. container adaptors -- stack,queue,priority queue
+ 3. associative containers -- 
+ 
+ 
+ 
+ 
+ /************* iterators *****************/
+
+ 
+
+
+*/
