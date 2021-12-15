83b358a3921eb3c18a7704ab7cb66754ab5d0356
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2021-12-15 21:44:01 +0530

    Create palindrome.cpp

---
+++ Arrays/character arrays/palindrome.cpp
@@ -0,0 +1,29 @@
+ 
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+bool ispalindrome( char a[])
+{
+	int i=0;
+	int j = strlen(a)-1;
+
+	while(i<j)
+	{
+		if(a[i]!=a[j])
+		   return false;
+        i++;
+        j--;   
+	}
+
+	return true;
+}
+
+int main() 
+{
+	char a[] = "aba";
+	cout<<ispalindrome(a)<<endl;
+
+	return 0;
+}
+
