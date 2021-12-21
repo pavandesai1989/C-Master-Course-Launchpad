4c6781a72d4dadf81fe33c356fa54b5906d4794e
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2021-12-21 20:29:27 +0530

    Update std Algorithms.cpp

--- std Algorithms.cpp
+++ std Algorithms.cpp
@@ -1,1 +1,11 @@
 1. find function : 
+    
+    array<int,5> arr{1,2,3,4,5};
+
+    auto it =  find(arr.begin(),arr.end(),5);
+    if(it != arr.end()) 
+    {
+           cout<<"element found at index "<<it-arr.begin()<<endl;
+    } 
+    else if (it==arr.end())     
+       cout<<"element not found "<<endl;  
