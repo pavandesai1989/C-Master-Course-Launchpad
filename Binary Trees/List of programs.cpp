aa33fe2eca5edca12675ff1616b725f8402464e7
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2022-01-01 22:01:44 +0530

    Update List of programs.cpp

--- Binary Trees/List of programs.cpp
+++ Binary Trees/List of programs.cpp
@@ -1,1 +1,7 @@
+/********* Trees *********/
+basic code to build and print tree. generic tree.
+inputtree function and print
+inputlevelwise and print tree
+print levelwise
+tree traversals. Both recursive and iterative
 
